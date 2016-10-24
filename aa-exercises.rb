5512a2016ba39c9354d0e80fd4e73b880efd6885
n <nevindnl@gmail.com>
2016-10-23 23:56:29 -0700

    update

--- aa-exercises.rb
+++ aa-exercises.rb
@@ -1,3 +1,18 @@
+def sum_to(n)
+	return nil if n < 1
+	n == 1 ? n : n + sum_to(n-1)
+end
+
+def add_numbers(arr = nil)
+	return nil if arr == nil
+	arr.length <= 1 ? arr[0] : arr[0] + add_numbers(arr[1..-1])
+end
+
+def gamma(n)
+	return nil if n < 1
+	n == 1 ? 1 : (n-1) * gamma(n - 1)
+end
+
 def range start_idx, end_idx
   return [start_idx] if end_idx <= 0
   range(start_idx, end_idx - 1) + [end_idx]
